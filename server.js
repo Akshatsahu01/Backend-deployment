b7ff3e09477e8170b429f91b75e431f68d3584cc
Akshat <akshat.sahu.s.125@kalvium.community>
2026-05-01 10:21:16 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express=require('express')
 const app=express()
-app.get("/",(res,req)=>{
+app.get("/",(req,res)=>
     res.status(200).json({message:"Hello word "})
-})
+)
 app.listen(3000,()=>{
     console.log("server is running")
 })
